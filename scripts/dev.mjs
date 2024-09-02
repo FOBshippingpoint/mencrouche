881dedf2d512c3a124b64738642c248ca7d37c26
CC Lan <60565355+FOBshippingpoint@users.noreply.github.com>
2024-09-03 04:05:13 +0800

    chore[no ci]: remove useless script

--- scripts/dev.mjs
+++
@@ -1,6 +0,0 @@
-import "zx/globals";
-import { getManifestString } from "../src/manifest.js";
-
-await $`mkdir -p dist`;
-const manifest = getManifestString(2);
-fs.writeFile("src/manifest.json", manifest);
